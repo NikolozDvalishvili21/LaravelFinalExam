54a8b83f8489fa5a7f51a848949f1c4f52c27a4a
Nika <nikadvalishvili517@gmail.com>
2025-01-24 23:43:31 +0400

    made route prefixes

--- athlete-injury-tracking/routes/web.php
+++ athlete-injury-tracking/routes/web.php
@@ -16,11 +16,13 @@ Route::put('athletes/{id}', [AthleteController::class, 'update'])->name('athlete
 Route::delete('athletes/{id}', [AthleteController::class, 'destroy'])->name('athletes.destroy');
 
 // Injury Routes
-Route::get('injuries', [InjuryController::class, 'index'])->name('injuries.index');
-Route::post('injuries', [InjuryController::class, 'store'])->name('injuries.store');
-Route::get('injuries/{id}', [InjuryController::class, 'show'])->name('injuries.show');
-Route::put('injuries/{id}', [InjuryController::class, 'update'])->name('injuries.update');
-Route::delete('injuries/{id}', [InjuryController::class, 'destroy'])->name('injuries.destroy');
+Route::prefix('api')->group(function () {
+    Route::get('/injuries', [InjuryController::class, 'index']);
+    Route::post('/injuries', [InjuryController::class, 'store']);
+    Route::get('/injuries/{id}', [InjuryController::class, 'show']);
+    Route::put('/injuries/{id}', [InjuryController::class, 'update']);
+    Route::delete('/injuries/{id}', [InjuryController::class, 'destroy']);
+});
 
 // Treatment Routes
 Route::get('treatments', [TreatmentController::class, 'index'])->name('treatments.index');
